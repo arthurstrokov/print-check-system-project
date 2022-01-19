7551fbfbbfadcd7f436616597d80e34b251a2156
arthurstrokov <39419954+arthurstrokov@users.noreply.github.com>
2022-01-19 14:43:28 +0300

    up

--- src/main/java/com/gmail/arthurstrokov/printcheck/model/Card.java
+++ src/main/java/com/gmail/arthurstrokov/printcheck/model/Card.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.Objects;
 
@@ -12,6 +13,7 @@ public class Card {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private BigDecimal discount;
 
     public Card() {
--- src/main/java/com/gmail/arthurstrokov/printcheck/model/Product.java
+++ src/main/java/com/gmail/arthurstrokov/printcheck/model/Product.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.Objects;
 
@@ -12,8 +13,10 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private String productName;
     private BigDecimal productDiscountPercentage;
+    @NotNull
     private BigDecimal productPrice;
 
     public Product() {
