e4ae460545885609dae3d0c50f96cdba47d84b08
Arthur Strokov <arthurstrokov@gmail.com>
2022-03-26 18:52:33 +0300

    Revert "up tests"
    This reverts commit ee5fc9129ef77b1199534f942e2f3e11c83a538f.

--- src/test/java/com/gmail/arthurstrokov/printcheck/PrintCheckApplicationTests.java
+++ src/test/java/com/gmail/arthurstrokov/printcheck/PrintCheckApplicationTests.java
@@ -2,11 +2,19 @@ package com.gmail.arthurstrokov.printcheck;
 
 import com.gmail.arthurstrokov.printcheck.service.CardService;
 import com.gmail.arthurstrokov.printcheck.service.InputService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.math.BigDecimal;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 @SpringBootTest
 class PrintCheckApplicationTests {
@@ -17,6 +25,17 @@ class PrintCheckApplicationTests {
 
     @Test
     void contextLoads() {
-        assertTrue(true);
+
+    }
+
+    @Test
+    void test() {
+        String inputFromFile = inputService.readFromSomewhere(Path.of("demoTest.txt"));
+        assertEquals(inputFromFile, "4-3 4-6 2-1 9-2 8-4 8-5 1-4 2-6 3-12 card-4");
+        Assertions.assertNotEquals(inputFromFile, "4-3 4-6 2-1");
+
+        List<String> inputValuesList = new ArrayList<>(Arrays.asList(inputFromFile.split(" ")));
+        BigDecimal cardDiscount = cardService.getCardDiscount(inputValuesList);
+        System.out.println(cardDiscount);
     }
 }
