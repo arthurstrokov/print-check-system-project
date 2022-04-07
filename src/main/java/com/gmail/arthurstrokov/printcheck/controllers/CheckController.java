a53d47109b621e3d1884a02097be9ef72921d99e
Arthur Strokov <arthurstrokov@gmail.com>
2022-04-07 22:59:47 +0300

    up replace constructor to @RequiredArgsConstructor in CheckController

--- src/main/java/com/gmail/arthurstrokov/printcheck/controllers/CheckController.java
+++ src/main/java/com/gmail/arthurstrokov/printcheck/controllers/CheckController.java
@@ -4,6 +4,7 @@ import com.gmail.arthurstrokov.printcheck.model.Product;
 import com.gmail.arthurstrokov.printcheck.model.Sale;
 import com.gmail.arthurstrokov.printcheck.repository.ProductRepository;
 import com.gmail.arthurstrokov.printcheck.service.SaleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,16 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class CheckController {
     @Autowired
     private final ProductRepository productRepository;
     @Autowired
     private final SaleService saleCalculationService;
-
-    public CheckController(ProductRepository productRepository, SaleService saleCalculationService) {
-        this.productRepository = productRepository;
-        this.saleCalculationService = saleCalculationService;
-    }
 
     @GetMapping("/check")
     public String check(@RequestParam(value = "productId", required = false, defaultValue = "1") String productId,
