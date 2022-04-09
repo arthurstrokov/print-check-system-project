a1256fe86928592c830b288b5d97f6662ecf7a14
Arthur Strokov <arthurstrokov@gmail.com>
2022-04-09 23:55:21 +0300

    up add ProductController

---
+++ src/main/java/com/gmail/arthurstrokov/printcheck/controllers/ProductController.java
@@ -0,0 +1,33 @@
+package com.gmail.arthurstrokov.printcheck.controllers;
+
+import com.gmail.arthurstrokov.printcheck.aspects.InspectingController;
+import com.gmail.arthurstrokov.printcheck.model.Product;
+import com.gmail.arthurstrokov.printcheck.repository.ProductRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Employee rest controller
+ *
+ * @author Arthur Strokov
+ */
+@RestController
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductRepository repository;
+
+    /**
+     * @return List of employees
+     */
+    @Operation(summary = "Get all products", description = "Return list of products")
+    @InspectingController
+    @GetMapping("/products")
+    List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+}
