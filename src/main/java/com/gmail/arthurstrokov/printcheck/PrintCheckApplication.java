5a20c254520b754719c0d5e5a8b3ce264158b8c7
Arthur Strokov <arthurstrokov@gmail.com>
2022-04-13 20:27:40 +0300

    add PrintCheckService.java up do refactor logic add printCheckFromJsonFile() method

--- src/main/java/com/gmail/arthurstrokov/printcheck/PrintCheckApplication.java
+++ src/main/java/com/gmail/arthurstrokov/printcheck/PrintCheckApplication.java
@@ -2,7 +2,6 @@ package com.gmail.arthurstrokov.printcheck;
 
 import com.gmail.arthurstrokov.printcheck.model.Card;
 import com.gmail.arthurstrokov.printcheck.model.Product;
-import com.gmail.arthurstrokov.printcheck.model.Sale;
 import com.gmail.arthurstrokov.printcheck.repository.CardRepository;
 import com.gmail.arthurstrokov.printcheck.repository.ProductRepository;
 import com.gmail.arthurstrokov.printcheck.service.*;
@@ -14,13 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Arthur Strokov
@@ -29,14 +24,10 @@ import java.util.Map;
 @SpringBootApplication
 @RequiredArgsConstructor
 public class PrintCheckApplication {
-
-    private final InputService inputService;
-    private final CardService cardService;
-    private final SaleService saleCalculationService;
-    private final ProductService productService;
-    private final PrintService printService;
     private final CardRepository cardRepository;
     private final ProductRepository productRepository;
+    private final PrintCheckService printCheckService;
+    private final PrintService printService;
 
     public static void main(String[] args) {
         SpringApplication.run(PrintCheckApplication.class, args);
@@ -47,26 +38,15 @@ public class PrintCheckApplication {
         return args -> {
             String fileName = "check.txt";
             Files.deleteIfExists(Path.of(fileName));
-
             // Create some Card/Product objects, add them in H2 DB
             List<Card> cardList = HelpfulUtils.randomCards();
             List<Product> productList = HelpfulUtils.randomProducts();
             cardRepository.saveAll(cardList);
             productRepository.saveAll(productList);
-
-            // Take values from somewhere
-            String input = inputService.readFromSomewhere(Path.of("demo.txt"));
-            // Add values to list
-            List<String> inputValuesList = new ArrayList<>(Arrays.asList(input.split(" ")));
-            // If card exists, get discount
-            BigDecimal cardDiscount = cardService.getCardDiscount(inputValuesList);
-            int sizeValuesList = cardDiscount.compareTo(BigDecimal.ZERO) > 0 ? inputValuesList.size() - 1 : inputValuesList.size();
-            // Get products from order
-            Map<Product, Integer> products = productService.getProducts(inputValuesList, sizeValuesList);
-            // Count products price sum
-            List<Sale> saleList = saleCalculationService.sale(products);
-            // Print result both in console and file
-            printService.totalCalculation(saleList, cardDiscount);
+            // Print check
+            printCheckService.printCheckFromJsonFile("inputValues.json");
+            printCheckService.printCheckFromTxtFile("demo.txt");
+            // Print links
             printService.printLinks();
         };
     }
---
+++ src/main/java/com/gmail/arthurstrokov/printcheck/service/PrintCheckService.java
@@ -0,0 +1,66 @@
+package com.gmail.arthurstrokov.printcheck.service;
+
+import com.gmail.arthurstrokov.printcheck.model.PurchaseData;
+import com.gmail.arthurstrokov.printcheck.model.Product;
+import com.gmail.arthurstrokov.printcheck.model.Sale;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Class that executes the receipt printing logic
+ *
+ * @author Arthur Strokov
+ */
+@Service
+@RequiredArgsConstructor
+public class PrintCheckService {
+    private final InputService inputService;
+    private final CardService cardService;
+    private final SaleService saleCalculationService;
+    private final ProductService productService;
+    private final PrintService printService;
+
+    public void printCheckFromTxtFile(String fileName) {
+        // Take values from file
+        String input = inputService.readFromSomewhere(Path.of(fileName));
+        // Add values to list
+        List<String> inputValuesList = new ArrayList<>(Arrays.asList(input.split(" ")));
+        // If card exists, get discount
+        BigDecimal cardDiscount = cardService.getCardDiscount(inputValuesList);
+        // List size
+        int sizeValuesList = cardDiscount.compareTo(BigDecimal.ZERO) > 0 ? inputValuesList.size() - 1 : inputValuesList.size();
+        // Get products from order
+        Map<Product, Integer> products = productService.getProducts(inputValuesList, sizeValuesList);
+        // Count products price sum
+        List<Sale> saleList = saleCalculationService.sale(products);
+        // Print result both in console and file
+        printService.totalCalculation(saleList, cardDiscount);
+    }
+
+    /**
+     * @param fileName name file with store values
+     */
+    public void printCheckFromJsonFile(String fileName) {
+        // Take values from Json file
+        PurchaseData purchaseData = inputService.readFromJson(Path.of(fileName));
+        // Add values to list
+        List<String> inputValuesList = new ArrayList<>(purchaseData.getProducts());
+        // If card exists, get discount
+        BigDecimal cardDiscount = cardService.getCardDiscount(purchaseData.getCard());
+        // List size
+        int sizeValuesList = inputValuesList.size();
+        // Get products from order
+        Map<Product, Integer> products = productService.getProducts(inputValuesList, sizeValuesList);
+        // Count products price sum
+        List<Sale> saleList = saleCalculationService.sale(products);
+        // Print result both in console and file
+        printService.totalCalculation(saleList, cardDiscount);
+    }
+}
