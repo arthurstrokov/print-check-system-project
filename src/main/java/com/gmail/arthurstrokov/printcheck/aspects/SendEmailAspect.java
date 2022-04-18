32962444493e9a327c52fef95abf538419f885bb
Arthur Strokov <arthurstrokov@gmail.com>
2022-04-18 19:50:32 +0300

    up SendEmailAspect.java

--- src/main/java/com/gmail/arthurstrokov/printcheck/aspects/SendEmailAspect.java
+++ src/main/java/com/gmail/arthurstrokov/printcheck/aspects/SendEmailAspect.java
@@ -1,5 +1,6 @@
 package com.gmail.arthurstrokov.printcheck.aspects;
 
+import com.gmail.arthurstrokov.printcheck.properties.EmailProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -9,22 +10,33 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+/**
+ * Java send email aspect service class
+ *
+ * @author Arthur Strokov
+ * @version 1.0
+ */
 @Slf4j
 @Aspect
 @Component
 @RequiredArgsConstructor
 public class SendEmailAspect {
     private final JavaMailSender javaMailSender;
+    private final EmailProperties emailProperties;
 
+    /**
+     * Method for sending emails
+     *
+     * @param joinPoint joinPoint
+     */
     @Around("@annotation(SendEmail)")
     public Object sendEmail(ProceedingJoinPoint joinPoint) throws Throwable {
         String methodName = joinPoint.getSignature().getDeclaringTypeName();
         log.info(methodName + " has been executed.");
         SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setTo("arthurstrokov@gmail.com", "arthurstrokov@yandex.ru");
-
-        msg.setSubject("Testing Spring Boot Print Check App");
-        msg.setText("Hello.\n" + methodName + " has been executed.");
+        msg.setTo(emailProperties.getEmailSendAddress().split(" "));
+        msg.setSubject(emailProperties.getEmailSubject());
+        msg.setText(emailProperties.getEmailText());
 
         log.info("Sending email");
         javaMailSender.send(msg);
